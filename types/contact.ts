81746aff4e54ccdd0958c7fe877243bf62821abb
SMFaysalHaque <smfaysalhaque007@gmail.com>
2023-09-21 19:32:01 +0600

    update email and mobile number validation.

--- types/contact.ts
+++ types/contact.ts
@@ -1,7 +1,16 @@
 export interface info {
     name: string
     mobile: string
+    email: string
     address: string
     message: string
     picked: string
+}
+export interface err {
+    errorName: string
+    errorMobile: string
+    errorEmail: string
+    errorAddress: string
+    errorMessage: string
+    errorPicked: string
 }
