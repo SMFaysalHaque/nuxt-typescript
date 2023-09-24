4b98b93bf4d1e88398c1144b719a1140ada68565
SMFaysalHaque <smfaysalhaque007@gmail.com>
2023-09-24 18:51:32 +0600

    new file name and file content updated

---
+++ pages/restaurants.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Navbar />
+        <div class="w-6/12 mx-auto my-5">
+            <Restaurant />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
